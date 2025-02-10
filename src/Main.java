5d77446188c00ac7d14a7ddf663c45f01fbf23bf
jose jimenez <jimenjos000@warren.k12.in.us>
2025-02-10 13:46:25 -0500

    kljn

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args){
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("E");
+        arrayList.add("D");
+        arrayList.add("C");
+        arrayList.add("B");
+        arrayList.add("A");
+        arrayList.remove("C");
+        Collections.sort(arrayList);
+        System.out.println("ArrayList: " + arrayList);
+    }
 }
